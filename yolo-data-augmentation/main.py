37d8977b600090cf05f1c6fd8c99443cda21c8f2
0152neich <duychien25102004@gmail.com>
2024-10-29 13:03:15 +0700

    feat: Implement YOLODataAugmentation class with augmentation methods
    feat: Add Add configuration file for augmentation parameters
    
    feat: Create main script to demonstrate data augmentation
    
    feat: Add .gitignore files

---
+++ yolo-data-augmentation/main.py
@@ -0,0 +1,33 @@
+import cv2
+from data_augmentation.Data_Augmentation import YOLODataAugmentation
+from configs.config import *
+
+if __name__ == "__main__":
+    augmenter = YOLODataAugmentation(IMAGE_PATH + "1.png")
+    augmented_images = augmenter.apply_all({
+        "width": IMG_WIDTH,
+        "height": IMG_HEIGHT,
+        "angle": ROTATION_ANGLE,
+        "blur_type": BLUR_TYPE,
+        "brightness": BRIGHTNESS_JITTER,
+        "contrast": CONTRAST_JITTER,
+        "saturation": SATURATION_JITTER,
+        "hue": HUE_JITTER,
+        "noise_mean": NOISE_MEAN,
+        "noise_sigma": NOISE_SIGMA,
+        "scale_fx": SCALE_FX,
+        "scale_fy": SCALE_FY,
+        "x_shift": X_SHIFT,
+        "y_shift": Y_SHIFT,
+        "ksize": KSIZE
+    })
+
+    for title, image in augmented_images.items():
+        cv2.imshow(title, image)
+
+    while True:
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('e'):
+            print("Chương trình đã dừng.")
+            break
+    cv2.destroyAllWindows()
